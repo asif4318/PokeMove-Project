a2574127e38c5370b51a06a8fe5dbf2a7238ea70
Asif Islam <asifislam510@gmail.com>
2023-04-23 12:07:28 -0400

    Added some functionality to search bar

--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import data from "./names.csv";
 
 const SearchBar = () => {
   const style = {
@@ -9,7 +10,36 @@ const SearchBar = () => {
     borderRadius: "8px",
     padding: "0.5rem",
   };
-  return <input style={style} type="text" placeholder="Search Move"></input>;
+
+  const [searchText, setSearchText] = useState("");
+
+  const handleChange = (evt) => {
+    setSearchText(evt.target.value);
+    console.log(searchText);
+    getName(searchText);
+  };
+
+  const getName = (searchString) => {
+    let names;
+    fetch(data)
+      .then((r) => r.text())
+      .then((text) => {
+        const rows = text.split("\n");
+        names = rows;
+      })
+      .then(() => {
+        console.log(names.filter((element) => element.includes(searchString)));
+      });
+  };
+
+  return (
+    <input
+      style={style}
+      type="text"
+      placeholder="Search Move"
+      onChange={handleChange}
+    ></input>
+  );
 };
 
 export default SearchBar;
