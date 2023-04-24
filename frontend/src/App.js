bca2b37df04fd578871a1cc2275f8b0ba0f0b5bb
Asif Islam <asifislam510@gmail.com>
2023-04-24 05:50:39 -0400

    Frontend and backend connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,42 @@
 import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 
+const Test = ({ text }) => {
+  return <p>{text}</p>;
+};
+
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [pokemonList, setList] = useState("");
+  const [splitList, setSplitList] = useState([]);
+
+  const handleChange = (evt) => {
+    setSearchText(evt.target.value);
+    // fetch(`http://localhost:5000/hashmap?move=${evt.target.value}/`, {
+    //   method: "get",
+    //   mode: "cors",
+    // }).then((response) => console.log(response));
+  };
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:5000/hashmap?move=${searchText}`)
+      .then((res) => res.json())
+      .then((data) => setList(data.pokemon));
+  }, [searchText]);
+
+  useEffect(() => {
+    if (pokemonList !== undefined) {
+      setSplitList(pokemonList.split("\n"));
+    }
+  }, [pokemonList]);
+
   return (
     <div className="App">
       <h1>PokeMove Dex</h1>
-      <SearchBar />
+      <SearchBar onChangeHandler={handleChange} />
+      <Test text={splitList[0]}></Test>
     </div>
   );
 }
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
-import data from "./names.csv";
+import React from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ onChangeHandler }) => {
   const style = {
     width: "25rem",
     height: "2vh",
@@ -11,33 +10,12 @@ const SearchBar = () => {
     padding: "0.5rem",
   };
 
-  const [searchText, setSearchText] = useState("");
-
-  const handleChange = (evt) => {
-    setSearchText(evt.target.value);
-    console.log(searchText);
-    getName(searchText);
-  };
-
-  const getName = (searchString) => {
-    let names;
-    fetch(data)
-      .then((r) => r.text())
-      .then((text) => {
-        const rows = text.split("\n");
-        names = rows;
-      })
-      .then(() => {
-        console.log(names.filter((element) => element.includes(searchString)));
-      });
-  };
-
   return (
     <input
       style={style}
       type="text"
       placeholder="Search Move"
-      onChange={handleChange}
+      onChange={onChangeHandler}
     ></input>
   );
 };
